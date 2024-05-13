e07a9a2757fb3c731eca42a7ddfff07d9aa785b5
adimond24 <adimond24@s.tooeletech.edu>
2024-05-13 14:26:49 -0600

    cool-branch

--- buttonProject/script.js
+++ buttonProject/script.js
@@ -1,1 +1,7 @@
- 
+ document.getElementByClass('large red button')
+ const newButton = document.createElement('button');
+newButton.textContent= 'Click me!';
+document.body.appendChild(newButton);
+newButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
